cae73b8834f93efa77063cb254797bb153d6a939
Rabdya Santhosh <santhoshr.be22@uceou.edu>
2022-06-16 21:06:02 +0530

    Remove extra variable

--- sum.java
+++ sum.java
@@ -3,14 +3,12 @@ public class sum {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x,y,z;
+        int x,y;
         System.out.println("Enter the value of x");
         x=sc.nextInt();
         System.out.println("Enter the value of y");
         y=sc.nextInt();
-        
-        z=x+y;
-        System.out.println("The Sum of "+x+" and "+y+" is "+z);
+        System.out.println("The Sum of "+x+" and "+y+" is "+(x+y));
         sc.close();
     }
 }
